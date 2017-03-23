f5e2882b02d46a3a1a13d8e8c765dff9839b6045
Nathan Bullock <nathanbullock@gmail.com>
2017-03-22 16:00:01 -0800

    easybuild switched to kingpin

--- cmd/curator/main.go
+++ cmd/curator/main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"os"
+
 	"github.com/nthnca/curator/config"
 	"github.com/nthnca/easybuild"
+	kingpin "gopkg.in/alecthomas/kingpin.v2"
 )
 
 func main() {
-	easybuild.Build(config.Path, config.ProjectID)
+	app := kingpin.New(
+		"curator",
+		"Photo organizational system that run in Google AppEngine")
+	easybuild.RegisterCommands(app, config.Path, config.ProjectID)
+	kingpin.MustParse(app.Parse(os.Args[1:]))
 }
