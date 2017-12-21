427642a1108d1091508e7f9a3ceaefef7d2ff2ed
Nathan Bullock <nathanbullock@gmail.com>
2017-12-21 00:00:00 +0000

    New mediainfo storage object

---
+++ data/mediainfo/mediainfo.go
@@ -0,0 +1,236 @@
+// Package mediainfo.
+package mediainfo
+
+import (
+	"context"
+	"crypto/md5"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"sync"
+
+	"cloud.google.com/go/storage"
+	"github.com/golang/protobuf/proto"
+	"github.com/nthnca/curator/data/message"
+	"google.golang.org/api/iterator"
+)
+
+const (
+	masterFileName = "master.md"
+)
+
+type MediaInfo struct {
+	bucketName string
+	data       message.MediaSet
+	index      map[[32]byte]int
+	files      []string
+	wait       sync.WaitGroup
+}
+
+// NewMediaInfo will load the Media objects from the given bucket and return a MediaInfo object
+// that will allow you to continue to add Media objects to this bucket.
+func NewMediaInfo(ctx context.Context, client *storage.Client, bucketName string) (*MediaInfo, error) {
+	var mi MediaInfo
+	mi.bucketName = bucketName
+	mi.index = make(map[[32]byte]int)
+
+	if err := load(ctx, client, bucketName, masterFileName, &mi.data); err != nil {
+		if err != storage.ErrObjectNotExist {
+			return nil, fmt.Errorf("loading masterfile: %v", err)
+		}
+	}
+
+	for i, m := range mi.data.Media {
+		if len(m.Key) != 32 {
+			log.Fatalf("Data corrupted? Wrong key length: %d", len(m.Key))
+		}
+
+		var key [32]byte
+		copy(key[:], m.Key)
+
+		mi.index[key] = i
+	}
+
+	ch := make(chan *message.Media)
+	var wg sync.WaitGroup
+
+	bucket := client.Bucket(bucketName)
+	for it := bucket.Objects(ctx, nil); ; {
+		obj, err := it.Next()
+		if err != nil {
+			if err == iterator.Done {
+				break
+			}
+			log.Fatalf("Failed to iterate through objects: %v", err)
+		}
+		if obj.Name == masterFileName {
+			continue
+		}
+
+		mi.files = append(mi.files, obj.Name)
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			var tmp message.Media
+			err := load(ctx, client, bucketName, obj.Name, &tmp)
+			if err != nil {
+				log.Fatalf("Failed to load: %v (%v)", err, obj.Name)
+			}
+			ch <- &tmp
+		}()
+	}
+
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+
+	for m := range ch {
+		mi.insertInternal(ctx, client, m, false)
+	}
+	mi.saveAll(ctx, client)
+	return &mi, nil
+}
+
+// Insert saves a new Media object. If this objects Key is the same as an existing object it will
+// replace it if its timestamp is newer, if this new object is older it will drop it.
+func (mi *MediaInfo) Insert(ctx context.Context, client *storage.Client, media *message.Media) {
+	mi.insertInternal(ctx, client, media, true)
+}
+
+// Flush does a full save and cleans up any singleton files.
+func (mi *MediaInfo) Flush(ctx context.Context, client *storage.Client) {
+	mi.saveAll(ctx, client)
+	mi.wait.Wait()
+}
+
+func (mi *MediaInfo) All() []*message.Media {
+	return mi.data.Media
+}
+
+/*
+func debugMedia(msg string, media *message.Media) {
+	log.Printf("debug: %s, %p, %v, %d", msg, media, media.Key[:4], media.TimestampSecondsSinceEpoch)
+}
+
+func debug(fmt string, a ...interface{}) {
+	log.Printf("debug: "+fmt, a...)
+}
+*/
+
+func (mi *MediaInfo) insertInternal(ctx context.Context, client *storage.Client, media *message.Media, write bool) {
+	// debugMedia("Insert", media)
+
+	var key [32]byte
+	if len(media.GetKey()) != 32 {
+		log.Fatalf("Nope")
+	}
+	copy(key[:], media.Key)
+
+	i, ok := mi.index[key]
+	if ok {
+		// debugMedia("Found", mi.data.Media[i])
+		if mi.data.Media[i].TimestampSecondsSinceEpoch >= media.TimestampSecondsSinceEpoch {
+			return
+		}
+		mi.data.Media[i] = media
+	} else {
+		mi.index[key] = len(mi.data.Media)
+		mi.data.Media = append(mi.data.Media, media)
+	}
+	// debug("Number of entries: %d", len(mi.data.Media))
+
+	if !write {
+		return
+	}
+
+	if len(mi.files) > 20 {
+		// TODO: We should just saveOne, and then in a go routine do the save all.
+		if err := mi.saveAll(ctx, client); err != nil {
+			log.Fatalf("save all %v", err)
+		}
+	} else {
+		mi.saveOne(ctx, client, media)
+	}
+}
+
+func load(ctx context.Context, client *storage.Client, bucketName, filename string, p proto.Message) error {
+	reader, err := client.Bucket(bucketName).Object(filename).NewReader(ctx)
+	if err != nil {
+		if err == storage.ErrObjectNotExist {
+			return err
+		}
+		return fmt.Errorf("creating new reader: %v", err)
+	}
+
+	slurp, err := ioutil.ReadAll(reader)
+	reader.Close()
+	if err != nil {
+		return fmt.Errorf("trying to read: %v", err)
+	}
+
+	if err := proto.Unmarshal(slurp, p); err != nil {
+		return fmt.Errorf("unmarshalling proto: %v", err)
+	}
+	return nil
+}
+
+func (mi *MediaInfo) save(ctx context.Context, client *storage.Client, bucketName, filename string, p proto.Message) error {
+	data, err := proto.Marshal(p)
+	if err != nil {
+		return fmt.Errorf("marshalling proto: %v", err)
+	}
+
+	wc := client.Bucket(bucketName).Object(filename).NewWriter(ctx)
+	checksum := md5.Sum(data)
+	wc.MD5 = checksum[:]
+	if _, err := wc.Write(data); err != nil {
+		return fmt.Errorf("writing data: %v", err)
+	}
+	if err := wc.Close(); err != nil {
+		return fmt.Errorf("closing file: %v", err)
+	}
+	return nil
+}
+
+func (mi *MediaInfo) saveAll(ctx context.Context, client *storage.Client) error {
+	if err := mi.save(ctx, client, mi.bucketName, masterFileName, &mi.data); err != nil {
+		return fmt.Errorf("saving proto: %v", err)
+	}
+
+	for i, _ := range mi.files {
+		mi.wait.Add(1)
+		filename := mi.files[i]
+		go func() {
+			err := client.Bucket(mi.bucketName).Object(filename).Delete(ctx)
+			if err != nil {
+				// This seems to fail sometimes, but still deletes...
+				// At any rate we don't really care.
+				// log.Printf("Failed to delete: %v", err)
+			}
+			mi.wait.Done()
+		}()
+	}
+	mi.files = []string{}
+
+	return nil
+}
+
+func (mi *MediaInfo) saveOne(ctx context.Context, client *storage.Client, m *message.Media) error {
+	data, err := proto.Marshal(m)
+	if err != nil {
+		return fmt.Errorf("marshalling proto: %v", err)
+	}
+
+	shasum := sha256.Sum256(data)
+	filename := fmt.Sprintf("%s.md", hex.EncodeToString(shasum[:]))
+
+	if err := mi.save(ctx, client, mi.bucketName, filename, m); err != nil {
+		return fmt.Errorf("saving proto: %v", err)
+	}
+
+	mi.files = append(mi.files, filename)
+	return nil
+}
---
+++ data/mediainfo/mediainfo_test.go
@@ -0,0 +1,118 @@
+package mediainfo
+
+import (
+	"context"
+	"crypto/sha256"
+	"fmt"
+	"log"
+	"testing"
+	"time"
+
+	"cloud.google.com/go/storage"
+	"github.com/nthnca/curator/data/message"
+	"google.golang.org/api/iterator"
+)
+
+func TestMediaInfo(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping test in short mode.")
+	}
+
+	bucketName := "test-bucket-mediainfo"
+
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+
+	cleanup(ctx, client, bucketName)
+	time.Sleep(2 * time.Second)
+
+	baseCapabilities(ctx, client, bucketName)
+}
+
+func cleanup(ctx context.Context, client *storage.Client, bucketName string) {
+	log.Printf("cleanup")
+	bucket := client.Bucket(bucketName)
+	for it := bucket.Objects(ctx, nil); ; {
+		obj, err := it.Next()
+		if err != nil {
+			if err == iterator.Done {
+				break
+			}
+			log.Fatalf("Failed to iterate through objects: %v", err)
+		}
+
+		err = client.Bucket(bucketName).Object(obj.Name).Delete(ctx)
+		if err != nil {
+			log.Fatalf("Failed to delete object: %v (%s)", err, obj.Name)
+		}
+	}
+}
+func baseCapabilities(ctx context.Context, client *storage.Client, bucketName string) {
+	insert := func(mi *MediaInfo, time int64, count int) {
+		log.Printf("Insert time: %d count: %d", time, count)
+		for i := 0; i < count; i++ {
+			var photo message.Media
+			shasum := sha256.Sum256([]byte(fmt.Sprintf("%v", i)))
+			photo.Key = shasum[:]
+			photo.TimestampSecondsSinceEpoch = time
+			mi.Insert(ctx, client, &photo)
+		}
+	}
+
+	check := func(mi *MediaInfo, time int64, count int) {
+		log.Printf("Check time: %d count: %d", time, count)
+		index := make(map[[32]byte]int)
+		for _, e := range mi.All() {
+			var key [32]byte
+			copy(key[:], e.Key)
+			index[key] = 1
+			if e.TimestampSecondsSinceEpoch != time {
+				log.Fatalf("Wrong entry value: %d", e.TimestampSecondsSinceEpoch)
+
+			}
+		}
+		if len(index) != count {
+			log.Fatalf("Wrong number of entries: %d (%d)", len(mi.All()), len(index))
+		}
+	}
+
+	log.Printf("baseCapabilities")
+	{
+		mi, err := NewMediaInfo(ctx, client, bucketName)
+		if err != nil {
+			log.Fatalf("NewMediaInfo failed: %v", err)
+		}
+
+		insert(mi, 1, 3)
+		check(mi, 1, 3)
+		insert(mi, 2, 3)
+		check(mi, 2, 3)
+		insert(mi, 1, 3)
+		check(mi, 2, 3)
+	}
+
+	// Note that at this point we don't even have a master file.
+	time.Sleep(2 * time.Second)
+	log.Printf("reload")
+	mi, err := NewMediaInfo(ctx, client, bucketName)
+	if err != nil {
+		log.Fatalf("NewMediaInfo failed: %v", err)
+	}
+
+	check(mi, 2, 3)
+
+	t := time.Now().UnixNano()
+	insert(mi, 3, 50)
+	mi.Flush(ctx, client)
+	log.Printf("Adding 50 entries took %v seconds",
+		float64(time.Now().UnixNano()-t)/1000000000.0)
+
+	mi, err = NewMediaInfo(ctx, client, bucketName)
+	if err != nil {
+		log.Fatalf("NewMediaInfo failed: %v", err)
+	}
+	check(mi, 3, 50)
+}
