6474536aa9ea575dd1782303fc2f1efdb41ca79d
Nathan Bullock <nathanbullock@gmail.com>
2017-12-26 19:04:18 +0000

    Improve stats

--- cmd/handler/statphotos/statphotos.go
+++ cmd/handler/statphotos/statphotos.go
@@ -3,10 +3,12 @@ package statphotos
 import (
 	"context"
 	"log"
+	"sort"
 
 	"cloud.google.com/go/storage"
 	"github.com/nthnca/curator/config"
 	"github.com/nthnca/curator/mediainfo/store"
+	"github.com/nthnca/curator/util"
 )
 
 func Handler() {
@@ -21,12 +23,37 @@ func Handler() {
 		log.Fatalf("New MediaInfo store failed: %v", err)
 	}
 
+	var totalsize int64
 	total := len(mi.All())
 	del := 0
+	yeart := make(map[string]int)
+	yeard := make(map[string]int)
+	years := make(map[string]int64)
 	for _, y := range mi.All() {
+		name := util.GetCanonicalName(y)
+		var size int64
+		for _, f := range y.File {
+			size += f.SizeInBytes
+		}
+
+		totalsize += size
+		years[name[:4]] += size
 		if y.Deleted {
+			yeard[name[:4]]++
 			del++
+		} else {
+			yeart[name[:4]]++
 		}
 	}
-	log.Printf("Photos %d (deleted: %d, total: %d)", total-del, del, total)
+	var keys []string
+	for k := range yeart {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	for _, k := range keys {
+		log.Printf("Photos %v: %v (%v) %d", k, yeart[k], yeard[k], years[k]/100000000)
+	}
+	log.Printf("Photos %d (deleted: %d, total: %d) %d",
+		total-del, del, total, totalsize/100000000)
 }
