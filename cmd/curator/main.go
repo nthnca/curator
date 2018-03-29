e27ac3d017e329b1f07097d3a8eea7f2e8327f21
Nathan Bullock <nathanbullock@gmail.com>
2018-03-29 00:00:03 +0000

    Remove unused app engine pieces

--- cmd/curator/main.go
+++ cmd/curator/main.go
@@ -10,8 +10,6 @@ import (
 	"github.com/nthnca/curator/cmd/handler/mutatephotos"
 	"github.com/nthnca/curator/cmd/handler/newphotos"
 	"github.com/nthnca/curator/cmd/handler/statphotos"
-	"github.com/nthnca/curator/config"
-	"github.com/nthnca/gobuild"
 	kingpin "gopkg.in/alecthomas/kingpin.v2"
 )
 
@@ -26,7 +24,6 @@ func main() {
 		"curator",
 		"Photo organizational system that run in Google AppEngine")
 	app.UsageWriter(os.Stdout)
-	gobuild.RegisterCommands(app, config.Path, config.ProjectID)
 	app.Flag("go", "Actually do things").BoolVar(&actual)
 
 	newphotos.Register(app, &actual)
