084d5852cf7b32db979a98d1b6cbbcdf1625b413
Nathan Bullock <nathanbullock@gmail.com>
2018-10-17 04:34:18 +0000

    Improve mutate

--- cmd/handler/mutatephotos/mutatephotos.go
+++ cmd/handler/mutatephotos/mutatephotos.go
@@ -1,17 +1,24 @@
 package mutatephotos
 
 import (
+	"bufio"
 	"context"
+	"encoding/hex"
+	"io"
+	"log"
+	"os"
+	"time"
 
 	"cloud.google.com/go/storage"
+	"github.com/golang/protobuf/proto"
+	"github.com/nthnca/curator/pkg/config"
+	"github.com/nthnca/curator/pkg/mediainfo/message"
 	"github.com/nthnca/curator/pkg/util"
+	objectstore "github.com/nthnca/object-store"
 	kingpin "gopkg.in/alecthomas/kingpin.v2"
 )
 
 var (
-	ctx    context.Context
-	client *storage.Client
-	// mediaInfo *store.MediaInfo
 	tags   util.Tags
 	actual *bool
 )
@@ -29,62 +36,77 @@ func Register(app *kingpin.Application, actualx *bool) {
 }
 
 func handler() {
-	/*
-	   	ctx := context.Background()
-	   	client, err := storage.NewClient(ctx)
-	   	if err != nil {
-	   		log.Fatalf("Failed to create client: %v", err)
-	   	}
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
 
-	   	mi, err := objectstore.New(ctx, client, config.MetadataBucket(), config.MetadataPath())
-	   	if err != nil {
-	   		log.Fatalf("New ObjectStore failed: %v", err)
-	   	}
+	mi, err := objectstore.New(ctx, client, config.MetadataBucket(), config.MetadataPath())
+	if err != nil {
+		log.Fatalf("New ObjectStore failed: %v", err)
+	}
 
-	   	tags.Normalize()
-	   	tags.Validate(config.ValidLabels())
+	tags.Normalize()
+	tags.Validate(config.ValidLabels())
 
-	   	log.Printf("--add %s", tags.A)
-	   	log.Printf("--remove %s", tags.B)
+	log.Printf("--add %s", tags.A)
+	log.Printf("--remove %s", tags.B)
 
-	   	reader := bufio.NewReader(os.Stdin)
-	   	for {
-	   		str, err := reader.ReadString('\n')
-	   		if err == io.EOF {
-	   			if str != "" {
-	   				mutate(mi, str)
-	   			}
-	   			break
-	   		}
-	   		if err != nil {
-	   			log.Fatalf("Failed to create client: %v", err)
-	   		}
+	objs := make(map[string][]byte)
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		str, err := reader.ReadString('\n')
+		if err == io.EOF {
+			if str != "" {
+				mutate(mi, objs, str)
+			}
+			break
+		}
+		if err != nil {
+			log.Fatalf("Failed to create client: %v", err)
+		}
 
-	   		// Strip newline off the end.
-	   		mutate(mi, str[:len(str)-1])
-	   	}
+		// Strip newline off the end.
+		mutate(mi, objs, str[:len(str)-1])
+	}
 
-	   	if *actual {
-	   		mi.Flush(ctx, client)
-	   	} else {
-	   		log.Printf("Running in dry run mode, no changes made")
-	   	}
-	   }
+	if *actual {
+		err := mi.InsertBulk(ctx, objs)
+		if err != nil {
+			log.Fatalf("Oops %v", err)
+		}
+	} else {
+		log.Printf("Running in dry run mode, no changes made")
+	}
+}
 
-	   func mutate(mi *objectstore.ObjectStore, line string) {
-	   	var changed bool
+func mutate(mi *objectstore.ObjectStore, objs map[string][]byte, line string) {
+	var changed bool
 
-	   	b, err := hex.DecodeString(line)
-	   	if err != nil {
-	   		log.Fatalf("Unable to decode sha256: %s", line)
-	   	}
-	   	p1 := mi.Get(util.Sha256(b))
-	   	p1.Tags, changed = tags.Modify(p1.Tags)
-	   	if changed {
-	   		p1.TimestampSeconds = time.Now().Unix()
-	   		mi.InsertFast(p1)
+	b, err := hex.DecodeString(line)
+	if err != nil {
+		log.Fatalf("Unable to decode sha256: %s", line)
+	}
 
-	   		log.Printf("Modifying %q\n", p1.File[0].Filename)
-	   	}
-	*/
+	value := mi.Get(hex.EncodeToString(b))
+
+	var p1 message.Media
+	if er := proto.Unmarshal(value, &p1); er != nil {
+		log.Fatalf("Unmarshalling proto: %v", er)
+	}
+
+	p1.Tags, changed = tags.Modify(p1.Tags)
+	if changed {
+		p1.TimestampSeconds = time.Now().Unix()
+
+		data, err := proto.Marshal(&p1)
+		if err != nil {
+			log.Fatalf("Failed to marshal proto")
+		}
+
+		objs[hex.EncodeToString(b)] = data
+
+		log.Printf("Modifying %q\n", p1.File[0].Filename)
+	}
 }
