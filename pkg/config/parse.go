02f6c9971407b9f89f597837ee1c77a1cbdc69e3
Nathan Bullock <nathanbullock@gmail.com>
2018-10-20 23:07:06 +0000

    Config is ready to go.

--- pkg/config/parse.go
+++ pkg/config/parse.go
@@ -10,11 +10,13 @@ import (
 	"github.com/nthnca/curator/pkg/config/internal"
 )
 
+// Config contains the users configuration settings for Curator.
 type Config struct {
 	instance     internal.CuratorConfig
 	cameraModels map[string]string
 }
 
+// CameraModelAbbreviation returns the users preferred short string for a given camera name.
 func (config *Config) CameraModelAbbreviation(name string) string {
 	m, ok := config.cameraModels[name]
 	if !ok {
@@ -23,35 +25,37 @@ func (config *Config) CameraModelAbbreviation(name string) string {
 	return m
 }
 
+// PhotoQueueBucket is the GCS bucket where new photos are stored.
 func (config *Config) PhotoQueueBucket() string {
 	return config.instance.PhotoQueueBucket
 }
 
+// PhotoQueueBucket is the GCS bucket where photos are stored after they are processed.
 func (config *Config) PhotoStorageBucket() string {
 	return config.instance.PhotoStorageBucket
 }
 
+// ValidLabels are the list of labels that a user can use to tag their photos.
 func (config *Config) ValidLabels() []string {
 	return config.instance.ValidLabels
 }
 
-func (config *Config) MediaInfoBucket() string {
-	return config.instance.PhotoInfoBucket
-}
-
+// MetadataBucket is the GCS bucket where photo metadata is stored.
 func (config *Config) MetadataBucket() string {
 	return config.instance.PhotoMetadataBucket
 }
 
+// MetadataBucket is the filename prefix for the photo metadata.
 func (config *Config) MetadataPath() string {
 	return config.instance.PhotoMetadataPath
 }
 
+// New parses the users configuration and returns a Config.
 func New() *Config {
-	config_file := os.Getenv("CONFIG_FILE")
+	configFile := os.Getenv("CONFIG_FILE")
 
-	log.Printf("Loading config from: %s", config_file)
-	data, err := ioutil.ReadFile(filepath.Join(config_file))
+	log.Printf("Loading config from: %s", configFile)
+	data, err := ioutil.ReadFile(filepath.Join(configFile))
 	if err != nil {
 		log.Fatalf("Failed to read config: %v", err)
 	}
