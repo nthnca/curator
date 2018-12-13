3db7d5088b9df8997a89b5eb810d45ee245d0587
Nathan Bullock <nathanbullock@gmail.com>
2018-12-12 16:00:00 -0800

    Simple bash script to dump SD card

---
+++ tools/dump_card.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+SRC="$1"
+DEST="$2"
+EXPENSIVE=0
+
+echo "Moving files from $SRC -> $DEST and deleting"
+echo
+sleep 3
+echo "PROCEEDING to copy and delete"
+
+function foo() {
+  echo PWD `pwd`
+  for file in *; do
+    if [[ "$file" == *jpg ]] || [[ "$file" == *JPG ]]; then
+      d="$DEST"
+      expensive=0
+    elif [[ "$file" == *rw2 ]] || [[ "$file" == *RW2 ]]; then
+      d="$DEST"/RAW
+      expensive=1
+    elif [[ "$file" == *MP4 ]] || [[ "$file" == *mp4 ]]; then
+      d="$DEST"/MP4
+      expensive=1
+    else
+      echo ERROR $file
+      exit 1
+    fi
+
+    mkdir -p $d
+    d=$d/"$file"
+
+    if [ -f "$d" ]; then
+      echo already exists "$d"
+      continue
+    fi
+
+    if [ "$EXPENSIVE" -eq "$expensive" ]; then
+      echo cp "$file" "$d"
+      cp "$file" "$d"
+      if [ $? -eq 0 ]; then
+        echo rm "$file"
+        rm "$file"
+      fi
+    fi
+  done
+}
+
+cd "$SRC"
+for a in 0 1; do
+  EXPENSIVE=$a
+  for dir in *; do
+    if [ -d "$dir" ]; then
+      cd "$dir"
+      foo
+      cd ..
+    fi
+  done
+done
